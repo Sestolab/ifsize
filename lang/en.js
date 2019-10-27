27beaec57688285a94cfe74c498310a222e0ab7c
Sestowner <owner@sestolab.pp.ua>
2019-10-27 11:00:06 +0200

    code formatting

--- lang/en.js
+++ lang/en.js
@@ -1,6 +1,6 @@
-CKEDITOR.plugins.setLang( 'ifsize', 'en', {
+CKEDITOR.plugins.setLang('ifsize', 'en', {
 	buttonLabel: 'Insert file size',
 	fileLabel: 'File',
 	fileEmpty: 'Please select a file',
 	decimalLabel: 'The number of decimal places'
-} );
+});
--- plugin.js
+++ plugin.js
@@ -2,7 +2,7 @@ CKEDITOR.plugins.add('ifsize', {
 	requires: 'dialog',
 	lang: 'en,ru,uk',
 	icons: 'ifsize',
-	init: function (editor) {
+	init: function (editor){
 		editor.addCommand('ifsize', new CKEDITOR.dialogCommand('ifsizeDialog'));
 		editor.ui.addButton('ifsize', {
 			label: editor.lang.ifsize.buttonLabel,
